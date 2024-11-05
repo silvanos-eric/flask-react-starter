0f1cfd365de61ce7abd38073adb5da675969a6b8
silvanos-eric <eric.ouma@student.moringaschool.com>
2024-11-05 09:02:18 +0300

    feat(server): initialize Flask application with basic API endpoint
    - Added Flask as a dependency in Pipfile.
    - Created `app.py` in the `server` directory to set up the Flask application.
    - Configured Flask to serve static files and templates from the React build directory.
    - Implemented a `/api/hello` endpoint that returns a JSON message.

---
+++ server/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__,
+            static_url_path='',
+            template_folder='../client/build',
+            static_folder='../client/build')
+
+
+@app.route('/api/hello')
+def hello():
+    return {'message': 'Hello, from Flask!'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
