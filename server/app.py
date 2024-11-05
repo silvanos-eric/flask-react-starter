e2662add11e57ca50af8315301f1089c0609a797
silvanos-eric <eric.ouma@student.moringaschool.com>
2024-11-05 14:32:33 +0300

    feat(server): add error handler for 404, update static paths and dependencies
    - **Add `404` error handler**:
      - Implemented `@app.errorhandler(404)` to render `index.html` for unmatched routes, improving
        routing compatibility with React in production.
    
    - **Update static and template paths**:
      - Changed `template_folder` and `static_folder` paths from `client/build` to `client/dist` in
        alignment with Vite build output.
    
    - **Add Gunicorn as production dependency**:
      - Included `gunicorn` in `Pipfile` to serve the Flask app in production environments.

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__,
             static_url_path='',
-            template_folder='../client/build',
-            static_folder='../client/build')
+            template_folder='../client/dist',
+            static_folder='../client/dist')
 
 
 @app.route('/api/hello')
 def hello():
     return {'message': 'Hello, from Flask!'}
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
